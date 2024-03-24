93a04c888011ef7875f217762ce4c07d0a53b42e
Tobey Blaber <contact@tobey.me>
2024-03-24 13:23:08 +0000

    Update AssemblyInfo.cs

--- Tobey.BepInExMelonLoaderWizard/Properties/AssemblyInfo.cs
+++ Tobey.BepInExMelonLoaderWizard/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Tobey.BepInExMelonLoaderWizard")]
-[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyCopyright("Copyright © Tobey Blaber 2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
